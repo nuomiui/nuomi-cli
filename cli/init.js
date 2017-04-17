f2cc2d5851bccafeebd95fca1353d24ea1c26341
memoryza <jincai.wang@foxmail.com>
2017-04-17 17:53:06 +0800

    初始化nuomi-cli

---
+++ cli/init.js
@@ -0,0 +1,38 @@
+'use strict'
+const exec = require('child_process').exec;
+const co = require('co');
+const prompt = require('co-prompt');
+const config = require('../template');
+const chalk = require('chalk');
+
+module.exports = () => {
+    co(function *() {
+        let projectName = yield prompt('Input Project name(输入项目名称): ');
+        let viewName = yield prompt('Input Template Render Technology(输入模板渲染技术)[react|vue]: ');
+        let cssName = yield prompt('Input css Technology(输入css技术)[css|less|scss]: ');
+
+        let gitUrl = 'git@github.com:nuomiui/nuomi-component.git';
+        let branch = `${viewName}-${cssName}`;
+
+        if (!config.js[viewName]) {
+            console.log(chalk.red('\n  Template Render not allow(您输入模板引擎技术不在支持列表中)!'))
+            process.exit()
+        }
+        if (!config.css[cssName]) {
+            console.log(chalk.red('\n  css technology not support(输入的css技术不在支持列表)!'))
+            process.exit()
+        }
+        let cmdStr = `git clone -b ${branch} ${gitUrl} ${projectName}`;
+        console.log(chalk.white('\n Start generating...'));
+
+        exec(cmdStr, (error, stdout, stderr) => {
+            if (error) {
+              console.log(error)
+              process.exit()
+            }
+            console.log(chalk.green('\n √ Generation completed!'))
+            console.log(`\n cd ${projectName} && npm install \n`)
+            process.exit()
+        });
+    });
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+####糯米组件化命令行
+
+
+	1、npm install nuomi-cli -g
+
+	2、nuomi-cli i
+
+<img src="./doc/img/cli.png"/>
+
+
+
+
+
+####v0.01
+1、远程仓库暂时仅仅支持react\less
