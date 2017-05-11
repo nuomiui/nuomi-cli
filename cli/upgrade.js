ad3ee02593a035121c39007db4c49ac2447ba07f
memoryza <jincai.wang@foxmail.com>
2017-05-11 16:02:36 +0800

    增加更新编译框架功能和更新文档

--- cli/upgrade.js
+++ cli/upgrade.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 const child_process = require('child_process');
 const exec = child_process.exec;
 const execSync = child_process.execSync;
@@ -9,7 +9,7 @@ const chalk = require('chalk');
 
 module.exports = () => {
     co(function *() {
-        let projectName = yield prompt('Input Project name(输入项目名称): ');
+        execSync('rm -rf upgrade');
         let viewName = yield prompt('Input Template Render Technology(输入模板渲染技术)[react|vue]: ');
         let cssName = yield prompt('Input css Technology(输入css技术)[css|less|scss]: ');
         viewName= viewName.trim();
@@ -25,15 +25,22 @@ module.exports = () => {
             console.log(chalk.red('\n  css technology not support(输入的css技术不在支持列表)!'));
             process.exit();
         }
-        let cmdStr = `git clone -b ${branch} ${gitUrl} ${projectName}`;
-        console.log(chalk.white('\n Start generating...'));
+        let cmdStr = `git clone -b ${branch} ${gitUrl} upgrade`;
+        console.log(chalk.white('\n Start upgrade...'));
         exec(cmdStr, (error, stdout, stderr) => {
             if (error) {
               console.log(error);
               process.exit();
             }
-            execSync(`cd ${projectName} && git branch -m ${branch} master && git remote remove origin`);
-            console.log(chalk.green('\n √ Generation completed!'));
+            let cmdList = ['cp -R upgrade/cli/ ./cli/',
+                'cp -R upgrade/audient/ ./audient/',
+                'cp -R upgrade/docs/ ./docs/',
+                'cp -R upgrade/src/common/libs/ ./src/common/libs/',
+                'cp -R upgrade/src/common/util/ ./src/common/util/',
+                'rm -rf upgrade'
+            ];
+            execSync(cmdList.join(' && '));
+            console.log(chalk.green('\n √ compeleted upgrade!'));
             console.log(`\n npm install \n`);
             process.exit(0);
         });
--- readme.md
+++ readme.md
@@ -1,15 +1,23 @@
 ####糯米组件化命令行
 
 
-	1、npm install nuomi-cli -g
+1. npm install nuomi-cli -g
 
-	2、nuomi-cli i
+2. nuomi-cli i
 
 <img src="./doc/img/cli.png"/>
 
 
 
+更新编译框架
+
+在本地项目根目录执行
+
+
+nuomi-cli u
+
 
 
 ####v0.01
-1、远程仓库暂时仅仅支持react\less
+
+1、远程仓库暂时仅仅支持react|vue\less
